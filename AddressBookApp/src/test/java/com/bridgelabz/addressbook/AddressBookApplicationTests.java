c995ab9855de6e3385908dfe8dfdbd592cdc63c8
Anchal2003 <sahuanchal22@gmail.com>
2025-03-07 15:24:41 +0530

    [Anchal sahu] Add UC2- Create a Rest Controller to demonstrate the various HTTP Methods and Connet to MYSQL Database.

--- AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
+++ AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Use @SpringBootTest
 @SpringBootTest
 class AddressBookApplicationTests {
 
