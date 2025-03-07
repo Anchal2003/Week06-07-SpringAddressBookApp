e04b16c3fb41abe4c12c489b923e6a64de0617e9
Anchal2003 <sahuanchal22@gmail.com>
2025-03-07 16:51:58 +0530

    [Anchal sahu] Add Section2-UC2- Introducing Services Layer to write business logic in AddressBook App.

--- AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
+++ AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
@@ -3,7 +3,10 @@ package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//Use @SpringBootTest Annotation
 @SpringBootTest
+
+//Create a class AddressBookApplicationTests
 class AddressBookApplicationTests {
 
 	@Test
