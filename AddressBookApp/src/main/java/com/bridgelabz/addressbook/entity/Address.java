8e8cf59ab7645a2c1c6bd6b9588731fd5c9b6e63
Anchal2003 <sahuanchal22@gmail.com>
2025-03-07 11:40:46 +0530

    [Anchal Sahu] Add Entity file for  mapping to the database table.

---
+++ AddressBookApp/src/main/java/com/bridgelabz/addressbook/entity/Address.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.addressbook.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Use different type of Annotations
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+//Create a class Address and make it Entity class
+public class Address {
+
+    //Use @Id Annotation
+    @Id
+
+    //Create private variable to store the details
+    private String fullName;
+    private String address;
+    private String city;
+    private String state;
+    private int zipcode;
+    private String phoneNO;
+}
