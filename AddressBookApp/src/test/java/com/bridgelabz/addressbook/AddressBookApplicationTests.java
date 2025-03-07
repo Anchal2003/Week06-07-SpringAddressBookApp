5e11859ecd2006e0b3164370b426adb7803a6a3d
Anchal2003 <sahuanchal22@gmail.com>
2025-03-07 16:34:00 +0530

    [Anchal Sahu] Add UC1 - Introducing DTO and Model to AddressBook App.

--- AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
+++ AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
@@ -3,7 +3,6 @@ package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//Use @SpringBootTest
 @SpringBootTest
 class AddressBookApplicationTests {
 
