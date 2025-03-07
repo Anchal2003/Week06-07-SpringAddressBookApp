72100085fe6cfd3f28aa64358f61508bcfa953bc
Anchal2003 <sahuanchal22@gmail.com>
2025-03-08 00:08:48 +0530

    [Anchal Sahu] Add UC5- Ability for the Services Layer to store the AddressBook Data.

--- AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
+++ AddressBookApp/src/test/java/com/bridgelabz/addressbook/AddressBookApplicationTests.java
@@ -3,9 +3,7 @@ package com.bridgelabz.addressbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//Use @SpringBootTest Annotation
 @SpringBootTest
-
 //Create a class AddressBookApplicationTests
 class AddressBookApplicationTests {
 
