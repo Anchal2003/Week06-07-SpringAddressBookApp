397b7ce5c41621f03dfd5c6e42a717707d396040
Anchal2003 <sahuanchal22@gmail.com>
2025-03-07 12:17:17 +0530

    [Anchal Sahu] Add UC1- Create a Address Book Spring Project to cater to REST Request from Address Book UI.

--- WelcomeToAddressBook.java
+++
@@ -1,6 +0,0 @@
-//Create a WelcomeToAddressBook to print the welcome statement
-public class WelcomeToAddressBook {
-    public static void main(String[] args) {
-        System.out.println("\"Welcome to the Address Book App!\"");
-    }
-}
